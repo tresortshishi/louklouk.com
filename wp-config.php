c11543a7a50f3c1829bfa5001d9c055afe389c06
goosy <tresortshishi@yahoo.com>
2012-08-15 16:34:58 +0200

    update config db adrress

--- wp-config.php
+++ wp-config.php
@@ -18,12 +18,25 @@
 /** The name of the database for WordPress */
 define('DB_NAME', 'louklouk');
 
-/** MySQL database username */
-define('DB_USER', 'root');
 
-/** MySQL database password */
-define('DB_PASSWORD', 'root');
 
+switch (gethostname()) {
+	case "ubuntu":
+		define('DB_USER', 'root');
+
+		/** MySQL database password */
+		define('DB_PASSWORD', 'tresor');
+		break;
+	
+	default:
+		
+		/** MySQL database username */
+		define('DB_USER', 'root');
+
+		/** MySQL database password */
+		define('DB_PASSWORD', 'root');
+		break;
+}
 
 
 /** MySQL hostname */
